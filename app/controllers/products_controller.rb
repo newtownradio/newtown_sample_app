29ab03cd58a4d590679dd4a0c7f0300ed14e7dae
Colin Ilgen <colin.ilgen@gmail.com>
2016-04-14 19:09:51 -0400

    changed products controller index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,14 @@ class ProductsController < ApplicationController
 
   # GET /products
   # GET /products.json
-  def index
-    @products = Product.all
-  end
+ def index
+ if params[:q]
+   search_term = params[:q]
+   @products = Product.where("name LIKE ?", "%#{search_term}%")
+ else
+   @products = Product.all
+ end
+ end
 
   # GET /products/1
   # GET /products/1.json
