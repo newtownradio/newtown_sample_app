cbb03cb355900a4dc9209f0c150aa0b2141bf336
Colin Ilgen <colin.ilgen@gmail.com>
2016-04-15 20:59:27 -0400

    pushing changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,16 @@
 class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
-  def contact_form(email, name, message)
-  @message = message
-    mail(:from => email,
-        :to => 'colin.ilgen@gmail.com',
-        :subject => "A new contact form message from #{name}")
-  end
-
+def contact_form(email, name, message)
+ @message = message
+ mail(:from => email,
+     :to => 'colin.ilgen@gmail.com',
+     :subject => "A new contact form message from #{name}")
+end
 
 def thank_you
-   @name = params[:name]
-   @email = params[:email]
-   @message = params[:message]
-   ActionMailer::Base.mail(:from => @email,
-       :to => 'colin.ilgen@gmail.com',
-       :subject => "A new contact form message from #{@name}",
-       :body => @message).deliver_now
- end
-
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@name, @email, @message).deliver
 end
