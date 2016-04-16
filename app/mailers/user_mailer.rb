8ac2cb299382893a396def3b67e7020ed10be2ca
Colin Ilgen <colin.ilgen@gmail.com>
2016-04-15 20:08:12 -0400

    attempting to send email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,18 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'your-email@example.com',
+        :to => 'colin.ilgen@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
 end
+
+def thank_you
+   @name = params[:name]
+   @email = params[:email]
+   @message = params[:message]
+   ActionMailer::Base.mail(:from => @email,
+       :to => 'colin.ilgen@gmail.com',
+       :subject => "A new contact form message from #{@name}",
+       :body => @message).deliver_now
+ end
+
