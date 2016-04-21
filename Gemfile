3367864dbc348d0ccd9c3adc38ba6f6d7bde9248
Colin Ilgen <colin.ilgen@gmail.com>
2016-04-21 18:08:29 -0400

    pushing changes from authorization lesson

--- Gemfile
+++ Gemfile
@@ -52,4 +52,5 @@ end
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'devise'
+  gem 'cancancan'
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :products
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
   get '/static_pages/landing_page'
 
   get 'static_pages/featured'
