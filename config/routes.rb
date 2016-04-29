cbcdc559375bb0edf686997f08e1e68b24a78929
Colin Ilgen <colin.ilgen@gmail.com>
2016-04-29 17:40:44 -0400

    changed routes code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   resources :products
-
 
   get '/static_pages/landing_page'
 
