65e7adb0aaf34c312dd5a9ecb2a474b48fd98761
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-06 12:42:25 -0400

    making all changes from yesterday going through the material to make any additional changes and re-saving all

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
+    $('.rating').raty( { path: '/assets/images/star-on.png', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets/images/star-on.png',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
