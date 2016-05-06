6df0949cacb5aa278fe9cffe466fde86d56cb4b8
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-06 19:04:27 -0400

    changed javascript and more

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets/images/star-off.png', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets/images/star-on.png',
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
