4f9fb07dda716011013f6c5d925e88fc5b339026
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-09 12:10:28 -0400

    added ability to can can in ability.rb

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,4 @@
 module CommentsHelper
 end
+
+<%= link_to 'Destroy', comment, method: :delete, data: { confirm: 'Are you sure?' } %>
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+    can :manage, :all
+    else
+    can :read, :all 
+    end
   end
-
+end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
@@ -32,4 +36,3 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-end
