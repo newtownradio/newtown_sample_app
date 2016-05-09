fb53d87bb2800944aa7a459624187cfeef46394f
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-09 10:23:04 -0400

    bundle installing pagination gem

--- Gemfile
+++ Gemfile
@@ -53,4 +53,6 @@ end
   gem 'spring'
   gem 'devise'
   gem 'cancancan'
+  ## Gemfile for Rails 3+, Sinatra, and Merb
+  gem 'will_paginate', '~> 3.1.0'
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,22 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
+  validates :name, presence: true
 
 def average_rating
  comments.average(:rating).to_f
 end
 
-
 end
+
+## perform a paginated query:
+@posts = Post.paginate(:page => params[:page])
+
+# or, use an explicit "per page" limit:
+Post.paginate(:page => params[:page], :per_page => 30)
+
+## render page links in the view:
+<%= will_paginate @posts %>
+
+
+
