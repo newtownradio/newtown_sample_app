b49b85d9acdb7bca93652041edf662a623f87aac
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-16 14:09:55 -0400

    changing search bar padding

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 describe Product do
-	context "when the product has coments" do
+	context "when the product has comments" do
 		before do
-			@product = Product.create!(name: "race bike")
+			@product = Product.create!(name: "Race Bike")
             @user = User.create!(:email, :password)
-            @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+            @product.comments.create!(rating: 1, user: @user, body: "Awful Bike!")
+            @product.comments.create!(rating: 3, user: @user, body: "Decent Bike!")
+            @product.comments.create!(rating: 5, user: @user, body: "Awesome Bike!")
         end         
-
-it "returns the average rating of all commments" do
+end
+    it "returns the average rating of all commments" do
 	expect(@product.average).to eq "3"
 end
 
+   it "is not valid" do
+   expect (Product.new(description: "Great App")).not_to be_valid
+end
+
+end
