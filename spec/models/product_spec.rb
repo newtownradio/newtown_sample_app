2b627d8b03f56d0c6a5d15b4f3aac485590a561d
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-16 19:17:11 -0400

    removing whitespace

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 describe Product do
-	context "when the product has comments" do
-		before do
-			@product = Product.create!(name: "Race Bike")
-            @user = User.create!(:email, :password)
-            @product.comments.create!(rating: 1, user: @user, body: "Awful Bike!")
-            @product.comments.create!(rating: 3, user: @user, body: "Decent Bike!")
-            @product.comments.create!(rating: 5, user: @user, body: "Awesome Bike!")
-        end         
-end
-    it "returns the average rating of all commments" do
-	expect(@product.average).to eq "3"
-end
+ before do
+   @product = Product.create!(name: "Race Bike")
+   @user = User.create(email: "test@test.com", password: "password")
+   @product.comments.create!(rating: 1, user: @user, body: "Awful Bike")
+   @product.comments.create!(rating: 3, user: @user, body: "Decent Bike")
+   @product.comments.create!(rating: 5, user: @user, body: "Awesome Bike!")
+ end
 
-   it "is not valid" do
-   expect (Product.new(description: "Great App")).not_to be_valid
-end
+ it "returns the average rating of all comments" do
+   expect(@product.average_rating).to eq 3
+ end
+
+ it "is not valid" do
+   expect(Product.new(description: "Great bike")).to_not be_valid
+ end
 
 end
