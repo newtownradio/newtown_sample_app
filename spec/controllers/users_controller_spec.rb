5ae2ec079dd05260d827b2b6d384d40450fb7067
Colin Ilgen <colin.ilgen@gmail.com>
2016-05-17 19:04:58 -0400

    uploading changes to git

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,21 +4,32 @@ describe UsersController, :type => :controller do
   
   before do
    @user = User.create!(email: "test@test.com", password: "password")
+   @user2 = User.create!(email: "test2@test.com", password: "2password")
    end
 
    describe "GET #show" do
      context "User is logged in" do
-   	 before sign_in @user 
+   	 before do
+   	 sign_in @user 
+   	end
+
      it "loads correct user details" do
         get :show, id: @user.id
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq @user
    	end
 
+     it "does not load the second user" do
+     	get :show, id: @user2.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+    end  
+    end   
+
    	context "No user is logged in" do
    		it "redirects to login" do
    			get :show, id: @user.id
-   			expect(response).to redirect_to(root_path)
+   			expect(response).to redirect_to (new_user_session_path)
    		end
    	end
 end
