89048def100a5f16684b9f29a36430815a32ddc6
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-14 08:10:24 -0400

    pushing changes to user_factory.rb

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do 
+	sequence(:email) { |n| "user#{n}@example.com" }
 	factory :user do 
-		email 
-		password 
-		first_name 
-		last_name 
+		email "user@example.com"
+		password "testpassword"
+		first_name "test"
+		last_name "user"
 		admin false
-
 	end
 end
 
 factory :admin, class: User do
-  email 
-  password 
+  email "colin_ilgen@icloud.com"
+  password "gemcore1981"
   admin true
-  first_name 
-  last_name 
+  first_name "colin"
+  last_name "ilgen"
 end
