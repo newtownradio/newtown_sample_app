c69185a37dd437e6633dd5df24cd7201f93ea899
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-18 17:23:03 -0400

    changed stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
 		  Rails.configuration.stripe = {
-		    :publishable_key => ENV['pk_test_kCrAjvgE32B3QsJAva3PReLz'],
-		    :secret_key => ENV['sk_test_xJhfiGJna976j9YcyjiFA625']
+		    :publishable_key => ENV[pk_live_tfxdzdMJNGdf1heLMRDDWCT5],
+		    :secret_key => ENV[sk_live_I5waNpGXHtkqDQ5rBqmvfJGW]
 		  }
 else
   Rails.configuration.stripe = {
-    :publishable_key => 'pk_live_tfxdzdMJNGdf1heLMRDDWCT5',
-    :secret_key => ' sk_live_I5waNpGXHtkqDQ5rBqmvfJGW'
-  }
+    :publishable_key => pk_test_kCrAjvgE32B3QsJAva3PReLz Roll Key
+,
+    :secret_key => sk_test_xJhfiGJna976j9YcyjiFA625 }
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
