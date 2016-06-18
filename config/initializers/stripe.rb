ed19e06a56c856809d79094650647f67ffadb5b7
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-18 17:25:30 -0400

    edit stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,8 @@ else
   Rails.configuration.stripe = {
     :publishable_key => pk_test_kCrAjvgE32B3QsJAva3PReLz Roll Key
 ,
-    :secret_key => sk_test_xJhfiGJna976j9YcyjiFA625 }
+    :secret_key => sk_test_xJhfiGJna976j9YcyjiFA625
+    } 
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
