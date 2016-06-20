c34beed4dd35578edd6724087923c3c8e5e4d934
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-20 08:40:02 -0400

    changing stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
 		  }
 else
   Rails.configuration.stripe = {
-    :publishable_key => [pk_test_kCrAjvgE32B3QsJAva3PReLz],
-    :secret_key => [sk_test_xJhfiGJna976j9YcyjiFA625]
+    :publishable_key => [pk_live_tfxdzdMJNGdf1heLMRDDWCT5],
+    :secret_key => [sk_live_I5waNpGXHtkqDQ5rBqmvfJGW]
     } 
 end
 
