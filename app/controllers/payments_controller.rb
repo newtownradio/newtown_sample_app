51a9967fed5ff2515e7ed8941b2a1befc382a8aa
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-24 07:57:15 -0400

    changing stripe code in payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,29 +12,38 @@ class PaymentsController < ApplicationController
   def create
   token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
-  @product=params[:product_id]
-  begin
-  @user=user_signed_in?
-  @user=current_user
-    charge = Stripe::Charge.create(
+  
+  # Create a Customer
+  customer = Stripe::Customer.create(
+    :source=> token,
+    :descrption => "Example customer"
+    )
+
+  # Charge the Customer instead of the card
+  
+   Stripe::Charge.create(
       :amount => 1000, # amount in cents, again
       :currency => "usd",
       :source => token,
-      :description => params[:stripeEmail]
-      :redirect_to @product_path(@product)
+      :customer => customer.id
     )
-      :description => params[:stripeEmail]
 
   if charge.paid
     Order.create()
   end
-...
+
+  Stripe::Charge.create(
+    :amount => 1500, # $15.00 this time
+    :currency => "usd",
+    :customer => customer_id #Previously stored, then retrieved
+    )
+
   rescue Stripe::CardError => e
     # The card has beenS declined
     body = e.json_body
     err = body[:error]
     flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
   end
-  redirect_to product_path(product)
+   product_path(product)
 end
 end
