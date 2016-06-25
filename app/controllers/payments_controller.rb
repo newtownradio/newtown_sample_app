b1ef56b518d0255e7c37316d2f05010cee3ee16e
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-25 10:59:37 -0400

    changed helper

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,8 +1,6 @@
 class PaymentsController < ApplicationController
 
 def create
-    @product = Product.find(params[:product_id])
-    @user = current_user
       token = params[:stripeToken]
       # Create the charge on Stripe's servers - this will charge the user's card
       begin
