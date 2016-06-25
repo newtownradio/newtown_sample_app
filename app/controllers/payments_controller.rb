e0ff0ce37c6cbc5e9c0db2a3860c50fb6d3829b1
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-25 09:05:05 -0400

    changed payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,39 +10,34 @@ class PaymentsController < ApplicationController
   end
 
   def create
+  @product = Product.find(parms[:product_id])
+  @user = current_user  
   token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
-  
-  # Create a Customer
-  customer = Stripe::Customer.create(
-    :source=> token,
-    :descrption => "Example customer"
-    )
-
-  # Charge the Customer instead of the card
-  
-   Stripe::Charge.create(
-      :amount => 1000, # amount in cents, again
-      :currency => "usd",
-      :source => token,
-      :customer => customer.id
-    )
-
-  if charge.paid
-    Order.create()
+  begin charge = Stripe::Charge.create(
+            :amount => @product.price, # amount in cents, again
+            :currency => "usd",
+            :source => token,
+            :description => params[:stripeEmail]
+           )
+​
+        if charge.paid
+          Order.create(
+            :product_id => @product.id,
+            :user_id => @user.id,
+            :total => @product.price
+            )
+        end
+​
+      rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      end
+​
+      redirect_to product_path(@product), notice: "Thank you for your purchase"
   end
-
-  Stripe::Charge.create(
-    :amount => 1500, # $15.00 this time
-    :currency => "usd",
-    :customer => customer_id #Previously stored, then retrieved
-    )
-
-  rescue Stripe::CardError => e
-    # The card has beenS declined
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-  end
-   product_path(product)
+​
+  
 end
