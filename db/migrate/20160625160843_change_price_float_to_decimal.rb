811ee8a193c2aa5491fc8a0b0727b9269fc27a91
Colin Ilgen <colin.ilgen@gmail.com>
2016-06-25 12:11:56 -0400

    changed migration file

---
+++ db/migrate/20160625160843_change_price_float_to_decimal.rb
@@ -0,0 +1,4 @@
+class ChangePriceFloatToDecimal < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509153432) do
+ActiveRecord::Schema.define(version: 20160625160843) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
