1b0425a26f301b52422d4385cb06cb69ed01c20b
Colin Ilgen <colin.ilgen@gmail.com>
2016-09-29 13:35:12 -0700

    pushing most recent changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, :email, :message, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
@@ -11,3 +12,4 @@ class AddAdminFlagToUsers < ActiveRecord::Migration
     add_column :users, :admin, :boolean, default: false, null: false
   end
 end
+
