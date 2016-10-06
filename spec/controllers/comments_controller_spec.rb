b7771bd8eda6c1e7962323342475497370e52b14
Colin Ilgen <colin.ilgen@gmail.com>
2016-10-05 18:30:10 -0700

    pushing final changes to demo

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe CommentsController, :type => :controller do
 
 before do
-  @product = FactoryGirl.create(Product.find([:id]))
-  @comment = FactoryGirl.create(@product.comments.new)
-  @user = FactoryGirl.create(user)
+@product = Product.create!(name: "Race Bike")
+  @user = User.create(email: "test@test.com", password: "password")
+  @product.comments.create!(rating: 1, user: @user, body: "Awful Bike")
   end
 
 context "GET #show"
   it "loads correct comment" do
-  expect(response).to eq have_http_status(200)
+  expect(response).to have_http_status(200)
   expect(response).to format.html { redirect_to @product, notice: 'Review was created successfully.' }
   expect(response).to format.json { render :show, status: :created, location: @product }
   expect(response).to format.js
 end
   it "does not load correct comment" do
-  expect(response).to eq have_http_status(422)
+  expect(response).to have_http_status(200)
   expect(response).to format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
   expect(response).to format.json { render json: @comment.errors, status: :unprocessable_entity }
 end
