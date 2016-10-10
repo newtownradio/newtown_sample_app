59f32d63800649b4cfac389ae968abf5b4fd847a
Colin Ilgen <colin.ilgen@gmail.com>
2016-10-10 13:11:02 -0700

    testing a few new lines in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,8 @@ class CommentsController < ApplicationController
    @comment.user = current_user
    respond_to do |format|
      if @comment.save
-       format.html { redirect_to @product, notice: 'Review was created successfully.' }
-       format.json { render :show, status: :created, location: @product }
+       format.html { redirect_to products_url, notice: 'Review was created successfully.' }
+       format.json { render :show, status: :created, location: @products }
        format.js
      else
        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
@@ -19,10 +19,17 @@ class CommentsController < ApplicationController
 	def destroy
   @comment = Comment.find(params[:id])
   product = @comment.product
-  @comment.destroy
-  redirect_to product
-	end
-
+  repond_to do |format|
+  if @comment.destroy
+    format.html { redirect_to products_url, notice: 'Comment was successfully destroyed.' }
+    format.json { render :show, status: :destroyed, location: @products }
+    format.js
+  else
+    format.html { redirect to @product, alert: 'Comment was not destroyed successfully.'}
+    format.json { render json: @comment.errors, status: :unprocessable_entity }
+   end
+  end
+end
 
 private
 
