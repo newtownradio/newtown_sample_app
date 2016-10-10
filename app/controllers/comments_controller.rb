63f6aa6a29f10509f91e510fef17c49de4f809ee
Colin Ilgen <colin.ilgen@gmail.com>
2016-10-10 15:31:40 -0700

    restoring changes made to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,49 +6,40 @@ class CommentsController < ApplicationController
    @comment.user = current_user
    respond_to do |format|
      if @comment.save
-       format.html { redirect_to products_url, notice: 'Success! Your review was created.' }
-       format.json { render :show, status: :created, location: products_url }
+       format.html { redirect_to products_url, notice: 'Review was created successfully.' }
+       format.json { render :show, status: :created, location: @products }
        format.js
      else
-       format.html { redirect_to @product, alert: 'Error: Unable to save review' }
+       format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
        format.json { render json: @comment.errors, status: :unprocessable_entity }
      end
    end
  end
-
-  def edit
-  @comment = Comment.find(params[:id])
-  @product = @comment.product
-  respond_to do |format|
-    if @comment.edit
-      format.html { redirect_to products_url, notice: 'Updated.' }
-      format.json { render :show, status: :updated, location: products_url }
-    else
-      format.html { redirect_to @products, alert: 'Error: Unable to update review.'}
-      format.json { render json: @comment.errors, status: :unprocessable_entity}
-    end
-  end
-end
 
 	def destroy
   @comment = Comment.find(params[:id])
   product = @comment.product
   repond_to do |format|
   if @comment.destroy
-    format.html { redirect_to products_url, notice: 'The review was deleted.' }
-    format.json { render :show, status: :destroyed, location: @comment.product }
+    format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+    format.json { render :show, status: :destroyed, location: @products }
     format.js
   else
-    format.html { redirect to @products, alert: 'Error: Unable to destroy review.'}
-    format.json { render json: @comment.errors, status: :unprocessable_entity }
+       format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+       format.json { render json: @comment.errors, status: :unprocessable_entity }
+     end
    end
-  end
+ end
+
+
 end
+
 
 private
 
   def comment_params
   	params.require(:comment).permit(:user_id, :body, :rating)
   end
+
 
 end
